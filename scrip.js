a90addbb82d6350cb605a75c070a0686a526a625
CristianTafur <tafurdiazcristianfelipe159@gmail.com>
2025-08-26 09:01:51 -0500

    Se agregó boton de eliminar registros

--- scrip.js
+++ scrip.js
@@ -1,10 +1,18 @@
+
 function cambiarTexto() {
     document.getElementById("titulo").innerText = "Hola gente que programa mucho!";
 
     const valor = document.getElementById("input").value;
 
-    const nuevoParrafo = document.createElement("p")
+    const nuevoParrafo = document.createElement("p");
     nuevoParrafo.innerText = valor;
 
-    document.getElementById("lista").appendChild(nuevoParrafo); 
+    document.getElementById("lista").appendChild(nuevoParrafo);
+
+    
+    document.getElementById("input").value = "";
+}
+
+function eliminarTodo() {
+    document.getElementById("lista").innerHTML = "";
 }
